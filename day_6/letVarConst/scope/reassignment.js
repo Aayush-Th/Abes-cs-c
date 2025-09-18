bd77c7699fbf28d1577526364d7d90e9d4f34b1d
Aayush-Th <thakuraayush633@gmail.com>
2025-09-18 12:41:39 +0530

    second commit

---
+++ day_6/letVarConst/scope/reassignment.js
@@ -0,0 +1,11 @@
+var a = 30;
+var a = 60;  //reassignment is possible in var
+console.log(a)
+
+
+let b = 14
+b = 20 //reassign is not possible whereas we change the value of b
+console.log(b)
+
+const pi = 3.14  //neither we reassign nor we can change the value of const
+console.log(pi)

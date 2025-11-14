f118f3350d31e66edf667cb2c1a81528a4cd83db
Aayush-Th <thakuraayush633@gmail.com>
2025-11-14 12:40:32 +0530

    input css

--- Todo-List/src/App.jsx
+++ Todo-List/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import InputToDo from "./Component/InputToDo"
+import './App.css';
 function App() {
   const [todoList, setTodoList] = useState([]);
   const inputHandler = (inputVal) => {
